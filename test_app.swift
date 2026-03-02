4fb868e1b22f29e98ee0355ef2fec5862db06a54
pc035860 <pc035860@gmail.com>
2026-03-03 04:48:57 +0800

    chore: remove accidental test files

--- test_app.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-let u = URL(fileURLWithPath: "/Applications/Safari.app")
-let isDir = (try? u.resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory
-let isPackage = (try? u.resourceValues(forKeys: [.isPackageKey]))?.isPackage
-print("isDir: \(String(describing: isDir))")
-print("isPackage: \(String(describing: isPackage))")
--- test_url.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-let u = URL(fileURLWithPath: "/tmp/foo")
-let u2 = u.appendingPathComponent(".")
-let u3 = u2.deletingLastPathComponent()
-print("original: \(u.path)")
-print("appended: \(u2.path)")
-print("deleted: \(u3.path)")
-
-let isDir = (try? u.resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory
